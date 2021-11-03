39ddf45ddfefe8bdc5c92c70d988dbf28895444c
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2021-11-02 20:39:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ web.listen(80, '0.0.0.0');
 
 const api = express();
 
-api.use(require('./endpoints/api'))
+api.use(require('./endpoints/api'));
 
 const server = api.listen(8080);
 
-require('./endpoints/ws')(server)
+require('./endpoints/ws')(server);
 
